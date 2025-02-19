be4fcd9abc66319fd9ed32b42eed4e17163ed9ec
sajjad-007 <sajjadhossain8123@gmail.com>
2025-02-19 15:22:02 +0600

    sub category done , node-11

--- src/route/index.js
+++ src/route/index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const route = express.Router()
 const authRoute = require("./auth.apiRoute")
 const categoryRoute = require("./category.api.Route")
+const subCategoryRoute = require("./subCategoryRoute")
 
 //.use() হল Express.js-এর একটি middleware ফাংশন,যা অন্য কোনো router যুক্ত করতে ব্যবহৃত হয়
 route.use("/home/api", authRoute);
 route.use("/home/api",categoryRoute);
+route.use("/home/api",subCategoryRoute)
 
 module.exports = {route}
